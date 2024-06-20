95976f7c974140ab04c26fbb1047545c2e3e3c3f
Glory_Kuria <kuriaglory@gmail.com>
2024-06-20 21:14:37 +0300

    add changes

--- app/src/main/java/com/akirachix/fibonaccinumbers/FibonacciRecyclerViewAdapter.kt
+++ app/src/main/java/com/akirachix/fibonaccinumbers/FibonacciRecyclerViewAdapter.kt
@@ -2,28 +2,26 @@ package com.akirachix.fibonaccinumbers
 
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+//import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class FibonacciRecyclerViewAdapter(private val fibonacciNumbers: List<Int>):
-    RecyclerView.Adapter<FibonacciRecyclerViewAdapter.viewHolder>(){
-        override fun onCreateViewHolder(parent:viewGroup,viewType:Int):viewHolder{
-            val view =
-                LayoutInflater.from(parent.context).inflate(R.layout.fibonacciseries,parent,false)
+  class FibonacciRecyclerViewAdapter(val fibonacciNumbers: List<Int>):
+    RecyclerView.Adapter<FibonacciRecyclerViewAdapter.ViewHolder>(){
+        override fun onCreateViewHolder(parent: ViewGroup, viewType:Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fibonacciseries,parent,false)
             return ViewHolder(view)
         }
-    override fun onBindViewHolder(holder:ViewHolder,position: Int){
-        holder.bind(fibonacciNumbers[position])
+     override fun onBindViewHolder(holder:ViewHolder,position: Int){
+        holder.tvName.text=fibonacciNumbers[position].toString()
     }
     override fun getItemCount():Int{
         return fibonacciNumbers.size
     }
-    class viewHolder(view: View):RecyclerView.ViewHolder(view){
-        private val textView:TextView = view.findViewById(R.id.fibonacci_text_view)
-        fun  bind(fibonacciNumber: Int){
-            textView.text = fibonacciNumber.toString()
-        }
+    class ViewHolder(view: View):RecyclerView.ViewHolder(view){
+        var tvName = view.findViewById<TextView>(R.id.tvName)
     }
 }
 
--- app/src/main/java/com/akirachix/fibonaccinumbers/MainActivity.kt
+++ app/src/main/java/com/akirachix/fibonaccinumbers/MainActivity.kt
@@ -15,31 +15,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root
-        )
+        setContentView(binding.root)
 
         val fibonacciGenerator = FibonacciGenerator()
         val fibonacciNumbers = fibonacciGenerator.generateFibonacciSequence(100)
-        binding.rvFibonacci.layoutManager=LinearLayoutManager(this)
-        val fibonacciAdapter =FibonacciRecyclerViewAdapter(fibonacciNumbers)
-    binding.rvFibonacci.adapter = fibonacciAdapter
-    }
-}
 
-class FibonacciGenerator {
-    fun generateFibonacciSequence(n: Int): List<Int> {
-        val fibSequence = mutableListOf(0, 1)
-        var a = 0
-        var b = 1
-        while (fibSequence.size < n) {
-            val sum = a + b
-            if (sum < 0) {
-                break
+
+        binding.rvFibonacci.layoutManager = LinearLayoutManager(this)
+        val fibonacciAdapter = FibonacciRecyclerViewAdapter(fibonacciNumbers)
+        binding.rvFibonacci.adapter = fibonacciAdapter
+    }
+
+
+    class FibonacciGenerator {
+        fun generateFibonacciSequence(n: Int): List<Int> {
+            val fibSequence = mutableListOf(0, 1)
+            var a = 0
+            var b = 1
+            while (fibSequence.size < n) {
+                val sum = a + b
+                if (sum < 0) {
+                    break
+                }
+                fibSequence.add(sum)
+                a = b
+                b = sum
             }
-            fibSequence.add(sum)
-            a = b
-            b = sum
+            return fibSequence
         }
-        return fibSequence
     }
 }
